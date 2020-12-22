4a33fca4e767ec1583f3243b09a218f58e15fcb0
Varatharuban B <developer-at-398378596368>
2020-12-22 19:48:55 +0800

    sayHi() method logic has changed.

--- src/main/java/com/rubanelearning/gitintro/controller/IndexController.java
+++ src/main/java/com/rubanelearning/gitintro/controller/IndexController.java
@@ -3,6 +3,10 @@ package com.rubanelearning.gitintro.controller;
 public class IndexController {
 
 	public void sayHi(String name) {
+		if(name.equals("Nila")) {
+			System.out.println("Hi Dr. " + name);
+			return;
+		}
 		System.out.println("Hi " + name);
 	}
 }
