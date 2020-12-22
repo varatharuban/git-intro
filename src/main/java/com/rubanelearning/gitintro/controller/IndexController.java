467eca6c8227dc7d53ca4e3664c7f386d3f361f4
Varatharuban B <developer-at-398378596368>
2020-12-22 21:01:53 +0800

    Order entity and calculateShipping() has added.

--- src/main/java/com/rubanelearning/gitintro/controller/IndexController.java
+++ src/main/java/com/rubanelearning/gitintro/controller/IndexController.java
@@ -6,18 +6,23 @@ import com.rubanelearning.gitintro.util.Constants;
 public class IndexController {
 
 	public void sayHi(String name) {
-		if(name.equals(Constants.NAME_NILA)) {
+		if (name.equals(Constants.NAME_NILA)) {
 			System.out.println("Hi Dr. " + name);
 			return;
 		}
 		System.out.println("Hi " + name);
 	}
-	
+
 	// Sabir
 	public void sayHello(String name) {
-		System.out.println("Hi " + name);	
+		System.out.println("Hi " + name);
 	}
-	
+
+	// Ninthu
+	public void sayThankYou(String name) {
+		System.out.println("Thank you " + name);
+	}
+
 	// By Ragi
 	public void calculateShipping(Order order) {
 		System.out.println(order.getProductId());
